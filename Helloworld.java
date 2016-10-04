4a1875b1c960f3c2be821419c40b1848f19e1669
itcastgit <1356153417@qq.com>
2016-10-05 00:02:32 +0800

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String string = scanner.nextLine();
+
+		System.out.println(string);
+		scanner.close();
+	}
+}
