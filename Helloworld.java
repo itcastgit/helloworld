0f0b5954b62fd13d0938d21ff4958bdd471e8c70
itcastgit <1356153417@qq.com>
2016-10-05 00:04:09 +0800

    修改了代码，输出hello  world
    修改了代码

--- Helloworld.java
+++ Helloworld.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Helloworld {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String string = scanner.nextLine();
+		//Scanner scanner = new Scanner(System.in);
+		//String string = scanner.nextLine();
 
-		System.out.println(string);
-		scanner.close();
+		System.out.println("hello world");
+		//scanner.close();
 	}
 }
